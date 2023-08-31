c47f3541a165651a4998c2244176189da6c4c3b3
song <sjin030@gmail.com>
2023-08-31 12:26:44 +0900

    08/31 main UI 버튼 이미지 추가

--- Source/Views/DialogLoading.py
+++ Source/Views/DialogLoading.py
@@ -1,9 +1,8 @@
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QMovie, QPixmap
+from PyQt5.QtGui import QMovie
 from PyQt5.QtCore import QByteArray, Qt
 
 from Source.Views.UI_DialogLoading import Ui_DlgLoading
-# from CLASS.Font import Font
 
 class DialogLoading(QWidget, Ui_DlgLoading):
     def __init__(self):
@@ -17,11 +16,3 @@ class DialogLoading(QWidget, Ui_DlgLoading):
         self.lbl_loading.setMovie(self.movie)
         self.movie.start()
 
-        # self.lbl_text.setFont(Font().button())
-
-    def center(self):
-        size = self.size()
-        height = 900
-        width = 600
-
-        self.move(int(width/2-size.width()/2), int(height/2 - size.height()/2))
